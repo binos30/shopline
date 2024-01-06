b7b20ee4613d12aea7840854d4f65f4083a0326e
Venus Lumanglas <vlumanglas30@gmail.com>
2024-01-06 23:54:08 +0800

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/assets/stylesheets/**/*.{css,scss}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./public/*.html"
+  ]
+};
