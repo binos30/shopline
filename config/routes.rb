22abbc2a496c4ee305c17fe5f35698dcc9085a1d
Venus Lumanglas <vlumanglas30@gmail.com>
2024-06-02 11:53:16 +0800

    Fix sign up

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,7 @@ Rails.application.routes.draw do
              },
              path_names: {
                sign_in: "/login",
-               sign_out: "/logout",
-               sign_up: "/new"
+               sign_out: "/logout"
              }
 
   get "admin" => "admin#index"
