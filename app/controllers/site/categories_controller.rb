423d79912a3b16008e4a7761810a7fe1bb9839b6
Venus Lumanglas <vlumanglas30@gmail.com>
2024-06-08 10:22:56 +0800

    Fetch active categories and products

--- app/controllers/site/categories_controller.rb
+++ app/controllers/site/categories_controller.rb
@@ -3,8 +3,8 @@
 module Site
   class CategoriesController < SiteController
     def show
-      @category = Category.includes(products: { images_attachments: :blob }).find(params[:slug])
-      @products = @category.products
+      @category = Category.active.find(params[:slug])
+      @products = @category.products.includes(images_attachments: :blob).active
     rescue ActiveRecord::RecordNotFound
       logger.error "Category not found #{params[:slug]}"
       redirect_back(fallback_location: root_url)
--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -3,7 +3,8 @@
 module Site
   class ProductsController < SiteController
     def show
-      @product = Product.includes([:stocks, { images_attachments: :blob }]).find(params[:slug])
+      @product =
+        Product.includes([:stocks, { images_attachments: :blob }]).active.find(params[:slug])
     rescue ActiveRecord::RecordNotFound
       logger.error "Product not found #{params[:slug]}"
       redirect_back(fallback_location: root_url)
