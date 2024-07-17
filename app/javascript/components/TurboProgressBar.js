4f843c319a0a3590a32add1ef2f9a054268c6e7d
Venus Lumanglas <vlumanglas30@gmail.com>
2024-07-17 11:21:43 +0800

    Add turbo progress bar

---
+++ app/javascript/components/TurboProgressBar.js
@@ -0,0 +1,13 @@
+export default class TurboProgressBar {
+  constructor() {
+    this.adapter = Turbo.navigator.delegate.adapter;
+  }
+
+  show() {
+    this.adapter.formSubmissionStarted(this);
+  }
+
+  hide() {
+    this.adapter.formSubmissionFinished(this);
+  }
+}
--- app/javascript/controllers/admin/table_controller.js
+++ app/javascript/controllers/admin/table_controller.js
@@ -1,10 +1,29 @@
 import { Controller } from "@hotwired/stimulus";
 import debounce from "debounce";
+import TurboProgressBar from "../../components/TurboProgressBar";
 
 // Connects to data-controller="admin--table"
 export default class extends Controller {
   initialize() {
     this.submitFilter = debounce(this.submitFilter.bind(this), 400);
+    this.turboProgressBar = new TurboProgressBar();
+  }
+
+  connect() {
+    // https://turbo.hotwired.dev/reference/events#turbo%3Abefore-fetch-request
+    document.addEventListener("turbo:before-fetch-request", () => {
+      this.turboProgressBar.show();
+    });
+
+    // https://turbo.hotwired.dev/reference/events#turbo%3Abefore-frame-render
+    document.addEventListener("turbo:before-frame-render", () => {
+      this.turboProgressBar.hide();
+    });
+
+    // https://turbo.hotwired.dev/reference/events#turbo%3Arender
+    document.addEventListener("turbo:render", () => {
+      this.turboProgressBar.hide();
+    });
   }
 
   submitFilter() {
