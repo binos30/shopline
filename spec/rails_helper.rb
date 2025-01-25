d85d0515c0b8a620f4b905b379547f1353903ebc
Venus Lumanglas <vlumanglas30@gmail.com>
2025-01-25 13:02:30 +0800

    Update SimpleCov configuration to use 'rails' for coverage tracking

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "simplecov"
-SimpleCov.start { add_filter("spec/support") }
+SimpleCov.start "rails"
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
